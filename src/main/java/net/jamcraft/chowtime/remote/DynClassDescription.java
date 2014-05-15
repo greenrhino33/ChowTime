9667bcf84b4e7f93606a3a97f38f42526707e0e7
James Hollowell <jamesthollowell@gmail.com>
2014-05-15 11:16:31 -0400

    Allout58: Automatic commit 12

---
+++ src/main/java/net/jamcraft/chowtime/remote/DynClassDescription.java
@@ -0,0 +1,10 @@
+package net.jamcraft.chowtime.remote;
+
+
+/**
+ * Created by James Hollowell on 5/15/2014.
+ */
+public class DynClassDescription
+{
+
+}
---
+++ src/main/java/net/jamcraft/chowtime/remote/Version.java
@@ -0,0 +1,21 @@
+package net.jamcraft.chowtime.remote;
+
+/**
+ * Created by James Hollowell on 5/15/2014.
+ */
+public class Version
+{
+    private int Major=0;
+    private int Minor=0;
+    private int 
+
+    public int getMajor()
+    {
+        return Major;
+    }
+
+    public int getMinor()
+    {
+        return Minor;
+    }
+}
