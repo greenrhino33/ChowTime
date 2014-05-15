3180a6b04d1f84f23eef0582000776726130df8d
James Hollowell <jamesthollowell@gmail.com>
2014-05-14 21:43:21 -0400

    Allout58: Automatic commit 7

---
+++ src/main/java/net/jamcraft/chowtime/dyn/DynItems.java
@@ -0,0 +1,19 @@
+package net.jamcraft.chowtime.dyn;
+
+import net.minecraft.item.Item;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by James Hollowell on 5/14/2014.
+ */
+public class DynItems
+{
+    public Map<String,Item> items=new HashMap<String,Item>();
+
+    public static void init()
+    {
+
+    }
+}
--- src/main/java/net/jamcraft/chowtime/dyn/common/IDynItem.java
+++ src/main/java/net/jamcraft/chowtime/dyn/common/IDynItem.java
@@ -1,8 +1,16 @@
 package net.jamcraft.chowtime.dyn.common;
 
+import net.minecraft.item.Item;
+
 /**
  * Created by James Hollowell on 5/14/2014.
  */
-public class IDynItem
+public interface IDynItem
 {
+    /**
+     * @return Name to register this item with Forge.
+     */
+    String getRegistrationName();
+
+    void registerRecipe();
 }
