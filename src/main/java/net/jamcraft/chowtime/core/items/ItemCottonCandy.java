d6ae8dd43c8ce345157db02e587d0cfa7f243c6e
kkaylium <kkaylium13@hotmail.com>
2014-05-15 05:30:31 -0600

    Cotton Caaannndddy!

---
+++ src/main/java/net/jamcraft/chowtime/core/items/ItemCottonCandy.java
@@ -0,0 +1,18 @@
+package net.jamcraft.chowtime.core.items;
+
+import net.jamcraft.chowtime.ChowTime;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
+
+/**
+ * Created by Kayla Marie on 5/15/14.
+ */
+public class ItemCottonCandy extends ItemFood{
+
+    public ItemCottonCandy(int p_i45339_1_, float p_i45339_2_, boolean p_i45339_3_) {
+        super(p_i45339_1_, p_i45339_2_, p_i45339_3_);
+        this.setCreativeTab(ChowTime.creativeTab);
+    }
+
+    
+}
