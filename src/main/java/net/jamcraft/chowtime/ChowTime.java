0c1324f3cdd4737e39ff2acd1ca94b86bf8d4dbc
James Hollowell <jamesthollowell@gmail.com>
2014-05-14 20:14:48 -0400

    Fix jam-craft to jamcraft in package...herp-a-derp

---
+++ src/main/java/net/jamcraft/chowtime/ChowTime.java
@@ -0,0 +1,11 @@
+package net.jamcraft.chowtime;
+
+import cpw.mods.fml.common.mod;
+
+/**
+ * Created by James Hollowell on 5/14/2014.
+ */
+@Mod(modid = ModConstants.MODID, name = ModConstants.NAME)
+public class ChowTime
+{
+}
---
+++ src/main/java/net/jamcraft/chowtime/core/ModConstants.java
@@ -0,0 +1,9 @@
+package net.jamcraft.chowtime.core;
+
+/**
+ * Created by James Hollowell on 5/14/2014.
+ */
+public class ModConstants
+{
+    public static String modname="chowtime";
+}
