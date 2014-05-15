a4fbfc788e3f7d3fa5affd202b785feb4d23a6a3
James Hollowell <jamesthollowell@gmail.com>
2014-05-15 12:36:21 -0400

    Allout58: Automatic commit 16

--- src/main/java/net/jamcraft/chowtime/remote/LooseObjList.java
+++ src/main/java/net/jamcraft/chowtime/remote/LooseObjList.java
@@ -33,4 +33,28 @@ public class LooseObjList
         Gson gson=new Gson();
         descriptions=gson.fromJson(jsonText,descriptions.getClass());
     }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if(!(o instanceof LooseObjList)) return false;
+
+        LooseObjList other=(LooseObjList)o;
+
+        //If they aren't the same size, they obviously aren't the same...
+        if(this.getObjects().size()!=other.getObjects().size()) return false;
+
+        for(int i=0;i<descriptions.size();i++)
+        {
+            if(!descriptions.contains(other.getObjects().get(i))) return false;
+        }
+
+        return true;
+    }
+
+    public List<DynClassDescription> difference(LooseObjList other)
+    {
+        List<DynClassDescription> diff=new ArrayList<DynClassDescription>();
+        for(DynClassDescription desc:)
+    }
 }
