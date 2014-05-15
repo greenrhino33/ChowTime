5122acbf38224d4d2e9ee31ce5ab87a61db6c3ed
kkaylium <kkaylium13@hotmail.com>
2014-05-14 21:36:15 -0600

    barley

---
+++ src/main/java/net/jamcraft/chowtime/core/crops/CropBarley.java
@@ -0,0 +1,13 @@
+package net.jamcraft.chowtime.core.crops;
+
+import net.minecraft.block.BlockCrops;
+
+/**
+ * Created by Kayla Marie on 5/14/14.
+ */
+public class CropBarley extends BlockCrops {
+
+    public CropBarley(){
+        super();
+    }
+}
---
+++ src/main/java/net/jamcraft/chowtime/core/items/SeedBarley.java
@@ -0,0 +1,15 @@
+package net.jamcraft.chowtime.core.items;
+
+import net.jamcraft.chowtime.ChowTime;
+import net.minecraft.item.ItemSeedFood;
+
+/**
+ * Created by Kayla Marie on 5/14/14.
+ */
+public class SeedBarley extends ItemSeedFood {
+
+    public SeedBarley(){
+        super();
+        this.setCreativeTab(ChowTime.creativeTab);
+    }
+}
