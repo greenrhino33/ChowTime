af90c66ef8e3f45dd06a99290903501cd822b5da
James Hollowell <jamesthollowell@gmail.com>
2014-05-15 13:21:31 -0400

    Allout58: Automatic commit 19

--- src/main/java/net/jamcraft/chowtime/remote/RemoteMain.java
+++ src/main/java/net/jamcraft/chowtime/remote/RemoteMain.java
@@ -1,18 +1,49 @@
 package net.jamcraft.chowtime.remote;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.gson.JsonIOException;
+import net.jamcraft.chowtime.ChowTime;
+import net.jamcraft.chowtime.core.Config;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by James Hollowell on 5/14/2014.
  */
 public class RemoteMain
 {
-    private static LooseObjList local=new LooseObjList();
-    private static LooseObjList remote=new LooseObjList();
+    private static LooseObjList local = new LooseObjList();
+    private static LooseObjList remote = new LooseObjList();
 
     public static void init()
     {
-        
+
+    }
+
+    public boolean LoadRemote()
+    {
+        try
+        {
+            URL url = new URL(Config.remoteLoc + "/dyn/current.json");
+            URLConnection con = url.openConnection();
+            InputStreamReader isr = new InputStreamReader(con.getInputStream());
+            BufferedReader br = new BufferedReader(isr);
+            String json = br.readLine();
+            remote.readFromJson(json);
+            return true;
+        }
+        catch (IOException e)
+        {
+            ChowTime.logger.error("Error reading remote JSON file; falling back to local only");
+        }
+        catch (JsonIOException je)
+        {
+            ChowTime.logger.error("Error parsing remote JSON file; falling back to local only");
+        }
+
+        return false;
     }
 }
