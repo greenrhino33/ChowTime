59c998259c2381f3d5398b34353367e17e1ee8ea
James Hollowell <jamesthollowell@gmail.com>
2014-05-14 20:46:15 -0400

    Allout58: Automatic commit 4

---
+++ src/main/java/net/jamcraft/chowtime/core/Config.java
@@ -0,0 +1,8 @@
+package net.jamcraft.chowtime.core;
+
+/**
+ * Created by James Hollowell on 5/14/2014.
+ */
+public class Config
+{
+}
---
+++ src/main/java/net/jamcraft/chowtime/core/client/ClientProxy.java
@@ -0,0 +1,10 @@
+package net.jamcraft.chowtime.core.client;
+
+import net.jamcraft.chowtime.core.CommonProxy;
+
+/**
+ * Created by James Hollowell on 5/14/2014.
+ */
+public class ClientProxy extends CommonProxy
+{
+}
