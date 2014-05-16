42b0896142708dfce9a9ed24168451275224f304
James Hollowell <jamesthollowell@gmail.com>
2014-05-16 17:13:46 -0400

    Allout58: Auto Commit Fri May 16 17:13:46 EDT 2014

--- src/main/java/net/jamcraft/chowtime/core/mobs/SeedMob/RenderSeedMob.java
+++ src/main/java/net/jamcraft/chowtime/core/mobs/SeedMob/RenderSeedMob.java
@@ -1,9 +1,23 @@
 package net.jamcraft.chowtime.core.mobs.SeedMob;
 
+import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
 
 /**
  * Created by Kayla Marie on 5/16/14.
  */
-public class RenderSeedMob extends RenderLiving{
+public class RenderSeedMob extends RenderLiving
+{
+
+    public RenderSeedMob(ModelBase par1ModelBase, float par2)
+    {
+        super(par1ModelBase, par2);
+    }
+
+    @Override protected ResourceLocation getEntityTexture(Entity var1)
+    {
+        return null;
+    }
 }
