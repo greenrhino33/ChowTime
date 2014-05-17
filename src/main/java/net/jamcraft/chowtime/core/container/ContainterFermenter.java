6e16975d2feb11dfbcf385f816a2c37d9d641f3c
James Hollowell <jamesthollowell@gmail.com>
2014-05-17 10:45:20 -0400

    Allout58: Auto Commit Sat May 17 10:45:21 EDT 2014

--- src/main/java/net/jamcraft/chowtime/core/container/ContainterFermenter.java
+++ src/main/java/net/jamcraft/chowtime/core/container/ContainterFermenter.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 /**
  * Created by James Hollowell on 5/16/2014.
@@ -38,5 +39,56 @@ public class ContainterFermenter extends Container
     @Override public boolean canInteractWith(EntityPlayer var1)
     {
         return true;
+    }
+
+    @Override
+    public ItemStack transferStackInSlot(EntityPlayer entityPlayer, int slotIndex)
+    {
+        ItemStack itemStack = null;
+        Slot slot = (Slot) inventorySlots.get(slotIndex);
+
+        if (slot != null && slot.getHasStack())
+        {
+
+            ItemStack slotItemStack = slot.getStack();
+            itemStack = slotItemStack.copy();
+
+            /**
+             * If we are shift-clicking an item out of the RocketAssembler
+             * container, attempt to put it in the first available slot in the
+             * player's inventory
+             */
+            if (slotIndex < TEFermenter.INV_SIZE)
+            {
+
+                if (!this.mergeItemStack(slotItemStack, TEFermenter.INV_SIZE, inventorySlots.size(), false))
+                {
+                    return null;
+                }
+            }
+            else
+            {
+
+                /**
+                 * If the stack being shift-clicked into the assembler's
+                 * container is a fuel, try to put it in the fuel slot.
+                 */
+                if (!this.mergeItemStack(slotItemStack, 0, TEFermenter.INV_SIZE, false))
+                {
+                    return null;
+                }
+            }
+
+            if (slotItemStack.stackSize == 0)
+            {
+                slot.putStack((ItemStack) null);
+            }
+            else
+            {
+                slot.onSlotChanged();
+            }
+        }
+
+        return itemStack;
     }
 }
