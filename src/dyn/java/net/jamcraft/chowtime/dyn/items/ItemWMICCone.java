290b60f3048536c4c33a8b8356e36e83c7f3bf55
James Hollowell <jamesthollowell@gmail.com>
2014-05-17 17:01:15 -0400

    Allout58: Auto Commit Sat May 17 17:01:15 EDT 2014

---
+++ src/dyn/java/net/jamcraft/chowtime/dyn/items/ItemWMICCone.java
@@ -0,0 +1,33 @@
+package net.jamcraft.chowtime.dyn.items;
+
+import net.jamcraft.chowtime.ChowTime;
+import net.jamcraft.chowtime.core.ModConstants;
+import net.jamcraft.chowtime.dyn.DynItems;
+import net.jamcraft.chowtime.dyn.common.IDynItem;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+
+/**
+ * Created by James Hollowell on 5/17/2014.
+ */
+public class ItemWMICCone extends ItemFood implements IDynItem
+{
+    public ItemWMICCone()
+    {
+        super(100, false);
+        setCreativeTab(ChowTime.creativeTab);
+        setTextureName(ModConstants.MODID + ":wmiccone");
+        setUnlocalizedName("wmiccone");
+    }
+
+    @Override public String getRegistrationName()
+    {
+        return "wmiccone";
+    }
+
+    @Override public void registerRecipe()
+    {
+        CraftingManager.getInstance().addShapelessRecipe(new ItemStack(this),new ItemStack(DynItems.items.get("cone")),new ItemStack(DynItems.items.get("wmicecream")));
+    }
+}
