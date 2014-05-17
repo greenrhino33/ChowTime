36cc278887c365f8576b94c7ad12e6d20648ccb9
kkaylium <kkaylium13@hotmail.com>
2014-05-16 18:01:12 -0600

    Such and Such

---
+++ src/main/java/net/jamcraft/chowtime/core/crops/CTCrops.java
@@ -0,0 +1,181 @@
+package net.jamcraft.chowtime.core.crops;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.jamcraft.chowtime.core.CTInits;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockBush;
+import net.minecraft.block.IGrowable;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by Kayla Marie on 5/16/14.
+ */
+public class CTCrops extends BlockBush implements IGrowable {
+
+    @SideOnly(Side.CLIENT)
+    private IIcon[] icons;
+
+    public CTCrops()
+    {
+        super(Material.plants);
+        float f = 0.5F;
+        this.setTickRandomly(true);
+        this.setBlockBounds(0.5F - f, 0.0F, 0.5F - f, 0.5F + f, 0.25F, 0.5F + f);
+        this.disableStats();
+    }
+
+    public int getRenderType()
+    {
+        return 6;
+    }
+
+    protected Item func_149866_i()
+    {
+        return CTInits.BarleySeeds;
+    }
+
+    public void dropBlockAsItemWithChance(World p_149690_1_, int p_149690_2_, int p_149690_3_, int p_149690_4_, int p_149690_5_, float p_149690_6_, int p_149690_7_)
+    {
+        super.dropBlockAsItemWithChance(p_149690_1_, p_149690_2_, p_149690_3_, p_149690_4_, p_149690_5_, p_149690_6_, 0);
+    }
+
+    @Override
+    public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune)
+    {
+        ArrayList<ItemStack> ret = super.getDrops(world, x, y, z, metadata, fortune);
+
+        if (metadata >= 7)
+        {
+            for (int i = 0; i < 3 + fortune; ++i)
+            {
+                if (world.rand.nextInt(15) <= metadata)
+                {
+                    ret.add(new ItemStack(this.func_149866_i(), 1, 0));
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int p_149691_1_, int p_149691_2_)
+    {
+        if (p_149691_2_ < 0 || p_149691_2_ > 7)
+        {
+            p_149691_2_ = 7;
+        }
+
+        return this.icons[p_149691_2_];
+    }
+
+    public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_)
+    {
+        return p_149650_1_ == 7 ? CTInits.BarleyCrop : CTInits.BarleySeeds;
+    }
+
+    public void updateTick(World p_149674_1_, int p_149674_2_, int p_149674_3_, int p_149674_4_, Random p_149674_5_)
+    {
+        super.updateTick(p_149674_1_, p_149674_2_, p_149674_3_, p_149674_4_, p_149674_5_);
+
+        if (p_149674_1_.getBlockLightValue(p_149674_2_, p_149674_3_ + 1, p_149674_4_) >= 9)
+        {
+            int l = p_149674_1_.getBlockMetadata(p_149674_2_, p_149674_3_, p_149674_4_);
+
+            if (l < 7)
+            {
+                float f = this.func_149864_n(p_149674_1_, p_149674_2_, p_149674_3_, p_149674_4_);
+
+                if (p_149674_5_.nextInt((int) (25.0F / f) + 1) == 0)
+                {
+                    ++l;
+                    p_149674_1_.setBlockMetadataWithNotify(p_149674_2_, p_149674_3_, p_149674_4_, l, 2);
+                }
+            }
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister p_149651_1_)
+    {
+        this.icons = new IIcon[8];
+
+        for (int i = 0; i < this.icons.length; ++i)
+        {
+            this.icons[i] = p_149651_1_.registerIcon(this.getTextureName() + "_stage_" + i);
+        }
+    }
+
+    private float func_149864_n(World p_149864_1_, int p_149864_2_, int p_149864_3_, int p_149864_4_)
+    {
+        float f = 1.0F;
+        Block block = p_149864_1_.getBlock(p_149864_2_, p_149864_3_, p_149864_4_ - 1);
+        Block block1 = p_149864_1_.getBlock(p_149864_2_, p_149864_3_, p_149864_4_ + 1);
+        Block block2 = p_149864_1_.getBlock(p_149864_2_ - 1, p_149864_3_, p_149864_4_);
+        Block block3 = p_149864_1_.getBlock(p_149864_2_ + 1, p_149864_3_, p_149864_4_);
+        Block block4 = p_149864_1_.getBlock(p_149864_2_ - 1, p_149864_3_, p_149864_4_ - 1);
+        Block block5 = p_149864_1_.getBlock(p_149864_2_ + 1, p_149864_3_, p_149864_4_ - 1);
+        Block block6 = p_149864_1_.getBlock(p_149864_2_ + 1, p_149864_3_, p_149864_4_ + 1);
+        Block block7 = p_149864_1_.getBlock(p_149864_2_ - 1, p_149864_3_, p_149864_4_ + 1);
+        boolean flag = block2 == this || block3 == this;
+        boolean flag1 = block == this || block1 == this;
+        boolean flag2 = block4 == this || block5 == this || block6 == this || block7 == this;
+
+        for (int l = p_149864_2_ - 1; l <= p_149864_2_ + 1; ++l)
+        {
+            for (int i1 = p_149864_4_ - 1; i1 <= p_149864_4_ + 1; ++i1)
+            {
+                float f1 = 0.0F;
+
+                if (p_149864_1_.getBlock(l, p_149864_3_ - 1, i1).canSustainPlant(p_149864_1_, l, p_149864_3_ - 1, i1, ForgeDirection.UP, this))
+                {
+                    f1 = 1.0F;
+
+                    if (p_149864_1_.getBlock(l, p_149864_3_ - 1, i1).isFertile(p_149864_1_, l, p_149864_3_ - 1, i1))
+                    {
+                        f1 = 3.0F;
+                    }
+                }
+
+                if (l != p_149864_2_ || i1 != p_149864_4_)
+                {
+                    f1 /= 4.0F;
+                }
+
+                f += f1;
+            }
+        }
+
+        if (flag2 || flag && flag1)
+        {
+            f /= 2.0F;
+        }
+
+        return f;
+    }
+
+    @Override
+    public boolean func_149851_a(World var1, int var2, int var3, int var4, boolean var5) {
+        return false;
+    }
+
+    @Override
+    public boolean func_149852_a(World var1, Random var2, int var3, int var4, int var5) {
+        return false;
+    }
+
+    @Override
+    public void func_149853_b(World var1, Random var2, int var3, int var4, int var5) {
+
+    }
+}
