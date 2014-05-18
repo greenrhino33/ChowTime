36c0f6601409a467bf9a6409dad1ea64232179f1
James Hollowell <jamesthollowell@gmail.com>
2014-05-17 23:05:50 -0400

    Add Strawberry Juice

--- src/dyn/java/net/jamcraft/chowtime/dyn/items/ItemStrawberryJuice.java
+++ src/dyn/java/net/jamcraft/chowtime/dyn/items/ItemStrawberryJuice.java
@@ -1,6 +1,7 @@
 package net.jamcraft.chowtime.dyn.items;
 
 import net.jamcraft.chowtime.ChowTime;
+import net.jamcraft.chowtime.core.CTInits;
 import net.jamcraft.chowtime.core.ModConstants;
 import net.jamcraft.chowtime.core.recipies.JuicerRecipes;
 import net.jamcraft.chowtime.dyn.common.IDynItem;
@@ -18,17 +19,17 @@ public class ItemStrawberryJuice extends Item implements IDynItem
     {
         super();
         setCreativeTab(ChowTime.creativeTab);
-        setTextureName(ModConstants.MODID + ":appleJuice");
-        setUnlocalizedName("appleJuice");
+        setTextureName(ModConstants.MODID + ":strawberryJuice");
+        setUnlocalizedName("strawberryJuice");
     }
 
     @Override public String getRegistrationName()
     {
-        return "applejuice";
+        return "strawberryJuice";
     }
 
     @Override public void registerRecipe()
     {
-        JuicerRecipes.AddRecipe(new ItemStack(Items.apple),new ItemStack(this),200);
+        JuicerRecipes.AddRecipe(new ItemStack(CTInits.Strawberry),new ItemStack(this),200);
     }
 }
