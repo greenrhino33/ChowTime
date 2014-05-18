6ca10939858adc1e16681f5b4f41e612aa912e59
kkaylium <kkaylium13@hotmail.com>
2014-05-17 19:21:58 -0600

    Strawberry Juice

--- src/main/java/net/jamcraft/chowtime/core/mobs/SeedMob/ModelSeedMob.java
+++ src/main/java/net/jamcraft/chowtime/core/mobs/SeedMob/ModelSeedMob.java
@@ -2,6 +2,7 @@ package net.jamcraft.chowtime.core.mobs.SeedMob;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelCow;
+import net.minecraft.client.model.ModelQuadruped;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.MathHelper;
