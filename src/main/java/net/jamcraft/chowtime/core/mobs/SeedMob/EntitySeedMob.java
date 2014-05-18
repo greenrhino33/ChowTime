00ca2c06b296e364e98a6c2070eb79707b559701
James Hollowell <jamesthollowell@gmail.com>
2014-05-18 15:28:54 -0400

    Allout58: Auto Commit Sun May 18 15:28:54 EDT 2014

--- src/main/java/net/jamcraft/chowtime/core/mobs/SeedMob/EntitySeedMob.java
+++ src/main/java/net/jamcraft/chowtime/core/mobs/SeedMob/EntitySeedMob.java
@@ -156,7 +156,6 @@ public class EntitySeedMob extends EntityAnimal
         Random random = new Random();
         int n = random.nextInt(9);
         int produce = random.nextInt(10) + 6;
-        if(){
         if (itemstack != null && itemstack.getItem() == Items.wheat_seeds && !par1EntityPlayer.capabilities.isCreativeMode)
         {
                 switch(n){
@@ -204,7 +203,6 @@ public class EntitySeedMob extends EntityAnimal
         else
         {
             return super.interact(par1EntityPlayer);
-        }
         }
     }
 
